1a40c3b4a0db8735acf25a50cd631af2e4bf364e
Sahil <sahil2002ece@gmail.com>
2023-09-10 13:30:03 +0530

    Hero Section is added

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,30 @@
+import './HeroSectionStyles.css';
+import HeroImg from '../assets/HeroImg3.jpg'
+import{Link} from 'react-router-dom';
+const HeroSection = () => {
+    return (
+        <>
+            <div className='hero'>
+                <div className='mask'>
+                    <img className='intro-img' src={HeroImg} alt="Intro-Img"></img>
+                </div>
+                <div className='content'>
+                    <p>Hello , My name is Sahil</p>
+                    <h1>A Full Stack developer</h1>
+                    <div>
+                        <Link to="/project"
+                            className="btn">
+                            Projects
+                        </Link>
+                        <Link to="/contact"
+                            className="btn btn-light">
+                            Contact
+                        </Link>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+export default HeroSection
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,9 @@
+import HeroSection from "../components/HeroSection"
 const Home=()=>{
     return(
-        <div><h2>Home</h2></div>
+        <div>
+            <HeroSection />
+        </div>
     )
 }
 export default Home
